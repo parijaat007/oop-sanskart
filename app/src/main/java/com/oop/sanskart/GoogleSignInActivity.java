3305980312680f3268a4577a192032e7df319288
parijaat007 <parijaatbmitra@gmail.com>
2021-03-27 15:59:23 +0530

    Added Basic Login Screen
    Only the layout, not functionality
    Plus started adding layout for the register screen

---
+++ app/src/main/java/com/oop/sanskart/GoogleSignInActivity.java
@@ -0,0 +1,6 @@
+package com.oop.sanskart;
+
+import com.google.android.gms.common.SignInButton;
+
+public class GoogleSignInActivity {
+}
--- app/src/main/java/com/oop/sanskart/MainActivity.java
+++ app/src/main/java/com/oop/sanskart/MainActivity.java
@@ -1,16 +1,28 @@
 package com.oop.sanskart;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    private Button joinNowButton,mainLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        joinNowButton=(Button) findViewById(R.id.join_now_button);
+        mainLoginButton=(Button) findViewById(R.id.main_login_button);
 
+        mainLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
