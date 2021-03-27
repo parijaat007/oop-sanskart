2b4082d61cc2d01156c1ac4de2b9a1e979788fe4
rutwiz <rutwijnerkar123@gmail.com>
2021-03-27 23:17:11 +0530

    Splash Screen Added

--- app/src/main/java/com/oop/sanskart/MainActivity.java
+++ app/src/main/java/com/oop/sanskart/MainActivity.java
@@ -2,27 +2,24 @@ package com.oop.sanskart;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button joinNowButton,mainLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        joinNowButton=(Button) findViewById(R.id.join_now_button);
-        mainLoginButton=(Button) findViewById(R.id.main_login_button);
-
-        mainLoginButton.setOnClickListener(new View.OnClickListener() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            public void run() {
                 startActivity(intent);
+                finish();
             }
-        });
+        }, 3000);
     }
 }
